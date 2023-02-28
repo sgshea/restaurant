59710e40b2ca17146a254f69f89dd9bd7a5e628a
sgshea <sammys512@yahoo.com>
2023-02-28 17:37:51 -0500

    feat: Create initial ingredients table and queries

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,1 +1,9 @@
--- Place your queries here. Docs available https://www.hugsql.org/
+-- :name create-ingredient! :! :n
+-- :doc creates a new ingredient
+INSERT INTO ingredients
+(name, amount)
+VALUES (:name, :amount)
+
+-- :name get-ingredients :? :*
+-- :doc selects all available ingredients
+SELECT * FROM ingredients

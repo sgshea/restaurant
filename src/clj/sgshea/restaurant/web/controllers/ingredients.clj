9b319228b311a705bb5051acdc93e723569e5e52
sgshea <sammys512@yahoo.com>
2023-02-28 18:00:01 -0500

    feat: backend support for ingredients

---
+++ src/clj/sgshea/restaurant/web/controllers/ingredients.clj
@@ -0,0 +1,24 @@
+(ns sgshea.restaurant.web.controllers.ingredients
+  (:require
+   [clojure.tools.logging :as log]
+   [sgshea.restaurant.web.routes.utils :as utils]
+   [ring.util.http-response :as http-response]))
+
+(defn create-ingredient!
+  [{{:strs [name amount]} :form-params :as request}]
+  (log/debug "saving ingredient" name amount)
+  (let [{:keys [query-fn]} (utils/route-data request)]
+    (try
+      (if (or (empty? name) (empty? amount))
+        (cond-> (http-response/found "/")
+                (empty? name)
+                (assoc-in [:flash :errors :name] "name is required")
+                (empty? amount)
+                (assoc-in [:flash :errors :name] "amount is required"))
+        (do
+          (query-fn :create-ingredient! {:name name :amount amount})
+          (http-response/found "/")))
+      (catch Exception e
+        (log/error e "failed to create ingredient!")
+        (-> (http-response/found "/")
+            (assoc :flash {:errors {:unknown (.getMessage e)}}))))))
--- src/clj/sgshea/restaurant/web/routes/pages.clj
+++ src/clj/sgshea/restaurant/web/routes/pages.clj
@@ -1,11 +1,13 @@
 (ns sgshea.restaurant.web.routes.pages
   (:require
-    [sgshea.restaurant.web.middleware.exception :as exception]
-    [sgshea.restaurant.web.pages.layout :as layout]
-    [integrant.core :as ig]
-    [reitit.ring.middleware.muuntaja :as muuntaja]
-    [reitit.ring.middleware.parameters :as parameters]
-    [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]))
+   [sgshea.restaurant.web.middleware.exception :as exception]
+   [sgshea.restaurant.web.pages.layout :as layout]
+   [integrant.core :as ig]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [reitit.ring.middleware.parameters :as parameters]
+   [ring.middleware.anti-forgery :refer [wrap-anti-forgery]]
+   [sgshea.restaurant.web.routes.utils :as utils]
+   [sgshea.restaurant.web.controllers.ingredients :as ingredients]))
 
 (defn wrap-page-defaults []
   (let [error-page (layout/error-page
@@ -13,12 +15,15 @@
                       :title "Invalid anti-forgery token"})]
     #(wrap-anti-forgery % {:error-response error-page})))
 
-(defn home [request]
-  (layout/render request "home.html"))
+(defn home [{:keys [flash] :as request}]
+  (let [{:keys [query-fn]} (utils/route-data request)]
+    (layout/render request "home.html" {:ingredients (query-fn :get-ingredients {})
+                                                       :errors (:errors flash)})))
 
 ;; Routes
 (defn page-routes [_opts]
-  [["/" {:get home}]])
+  [["/" {:get home}]
+   ["/create-ingredient" {:post ingredients/create-ingredient!}]])
 
 (defn route-data [opts]
   (merge
